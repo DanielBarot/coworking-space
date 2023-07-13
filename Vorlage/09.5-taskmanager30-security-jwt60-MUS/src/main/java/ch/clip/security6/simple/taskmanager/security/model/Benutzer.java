b7c7d97c5e4f648a835288575e613b5bba1a3351
V3rlassen <78602042+V3rlassen@users.noreply.github.com>
2023-07-13 14:02:38 +0200

    Entitäten

---
+++ Vorlage/09.5-taskmanager30-security-jwt60-MUS/src/main/java/ch/clip/security6/simple/taskmanager/security/model/Benutzer.java
@@ -0,0 +1,19 @@
+package ch.clip.security6.simple.taskmanager.security.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Benutzer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ Vorlage/09.5-taskmanager30-security-jwt60-MUS/src/main/java/ch/clip/security6/simple/taskmanager/security/model/Platz.java
@@ -0,0 +1,20 @@
+package ch.clip.security6.simple.taskmanager.security.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Platz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private boolean besetzt;
+}
